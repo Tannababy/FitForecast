52e41e0270e002196fb73c4bce7b924e56209bd0
Alexandra Asanache <ale.asanache@gmail.com>
2023-12-20 18:59:12 +0000

    Added app.js pseudocode

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//1. Get elements from HTML
+
+//2. Set empty array of objects
+
+//3. If history = true, display array of objects (showPrevWorkout) from history in 'past workouts' section.
+
+//4. Function showPrevWorkout to display max 3 workouts stored in local storage.
+
+//5. Pull today's date and display it - Day.js
+
+//6. Call the getWeather function from weather.js
+
+//7. Event handler for modalBtn to look at weather and display modal depending on the weather - chance of precip - if more than 20% get indoor workout , if less than 20% show outdoor workout
+
+//8. Once exercise is displayed (getExercise), store data to array of objects in local storage
