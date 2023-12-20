b2450e92d14c35e6ceb93f25f20cc38510084050
alinakristy <alina.cristi12@gmail.com>
2023-12-20 19:29:28 +0000

    add instruction for execises JS

---
+++ js/exercises.js
@@ -0,0 +1,13 @@
+//1. creating variables for HTML
+
+//2.filter exercises by user input
+
+//3.call  API
+
+//4.indoor workout call API 1
+
+//5.outdoot workout ,call function to display generic exercises(running , walking , cycle)+ callories burn API call
+
+//6.display data from indoor workout
+
+//7.display data from outdoor workout
