3a23439cc662facf8a94ed1ebebc90f375f45d8c
Lotanna <tannaloamobi@gmail.com>
2023-12-20 19:12:51 +0000

    Added pseudocode to weather.js file

---
+++ js/weather.js
@@ -0,0 +1,14 @@
+// 1. Create variables for the HTML elements we will use here
+
+// 2. Get geolocation
+
+//3. Set London to default location, if not allow user to input location
+
+//4. Convert geolocation data to latitude and longitude - API fetch
+
+//5. GetWeather - Fetch the weather API data
+
+//5. Call the getWeather function from weather.js
+
+//6. Display the getWeather (data) in the weather card
+//data - temperature, % of precipitation, weather icon
