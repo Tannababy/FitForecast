dd666c5738622bde7b21a31ccec0ba3276b1ceca
Lotanna <tannaloamobi@gmail.com>
2023-12-21 20:46:41 +0000

    added small commit meessage

--- js/app.js
+++ js/app.js
@@ -12,14 +12,15 @@
 
 //7. Event handler for modalBtn to look at weather and display modal depending on the weather - chance of precip - if more than 20% get indoor workout , if less than 20% show outdoor workout
 
-  // Call the getExercise function passing in two variables.
-    // The first variable should contain a string matching one of the bodyPart options
-    // The second variable should contain a string matching one of the equipment options
-      let bodyPart = 'cardio'; // This needs to be set to body part user input
-      let equipment = "body weight"; // This needs to be set to available equipment user input
-      getExercises(bodyPart, equipment);
-        console.log(exerciseName);
-        console.log(exerciseGif);
-        console.log(exerciseInstructions);
+// Call the getExercise function passing in two variables.
+// The first variable should contain a string matching one of the bodyPart options
+// The second variable should contain a string matching one of the equipment options
+let bodyPart = "cardio"; // This needs to be set to body part user input
+let equipment = "body weight"; // This needs to be set to available equipment user input
+getExercises(bodyPart, equipment);
+console.log(exerciseName);
+console.log(exerciseGif);
+console.log(exerciseInstructions);
 
 //8. Once exercise is displayed (getExercise), store data to array of objects in local storage
+var nothing = [];
