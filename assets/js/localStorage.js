090b8fada7fceb158892e3377d726349ee37f520
Alexandra Asanache <ale.asanache@gmail.com>
2024-01-02 18:29:18 +0000

    Fixed bug to show past exercise slider on click

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -1,8 +1,16 @@
 const pastExerciseSlider = $("#pastExerciseSlider");
 
-let savedExerciseTitle = localStorage.getItem("savedExerciseTitle") ? JSON.parse(localStorage.getItem("savedExerciseTitle")) : [];
-let savedExerciseInstructions = localStorage.getItem( "savedExerciseInstructions" ) ? JSON.parse(localStorage.getItem("savedExerciseInstructions")) : [];
-let savedExerciseGif = localStorage.getItem("savedExerciseGif") ? JSON.parse(localStorage.getItem("savedExerciseGif")) : [];
+let savedExerciseTitle = localStorage.getItem("savedExerciseTitle")
+  ? JSON.parse(localStorage.getItem("savedExerciseTitle"))
+  : [];
+let savedExerciseInstructions = localStorage.getItem(
+  "savedExerciseInstructions"
+)
+  ? JSON.parse(localStorage.getItem("savedExerciseInstructions"))
+  : [];
+let savedExerciseGif = localStorage.getItem("savedExerciseGif")
+  ? JSON.parse(localStorage.getItem("savedExerciseGif"))
+  : [];
 
 while (savedExerciseTitle.length > 6) {
   savedExerciseTitle.pop();
@@ -11,16 +19,19 @@ while (savedExerciseTitle.length > 6) {
 }
 
 localStorage.setItem("savedExerciseTitle", JSON.stringify(savedExerciseTitle));
-localStorage.setItem( "savedExerciseInstructions", JSON.stringify(savedExerciseInstructions));
+localStorage.setItem(
+  "savedExerciseInstructions",
+  JSON.stringify(savedExerciseInstructions)
+);
 localStorage.setItem("savedExerciseGif", JSON.stringify(savedExerciseGif));
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-const pastTitleOne = $('#pastTitleOne');
-const pastTitleTwo = $('#pastTitleTwo');
-const pastTitleThree = $('#pastTitleThree');
+const pastTitleOne = $("#pastTitleOne");
+const pastTitleTwo = $("#pastTitleTwo");
+const pastTitleThree = $("#pastTitleThree");
 
 pastTitleOne.text(capitalizeFirstLetter(savedExerciseTitle[0]));
 pastTitleTwo.text(capitalizeFirstLetter(savedExerciseTitle[1]));
@@ -30,50 +41,56 @@ const buttonOne = $("#pastOne");
 const buttonTwo = $("#pastTwo");
 const buttonThree = $("#pastThree");
 
-const pastTitle = $('#pastTitle');
-const pastInstructions = $('#pastInstructions');
-const pastGif = $('#pastGif');
-buttonOne.on('click', function(event) {
-    pastTitle.text(capitalizeFirstLetter(savedExerciseTitle[0]));
+const pastTitle = $("#pastTitle");
+const pastInstructions = $("#pastInstructions");
+const pastGif = $("#pastGif");
+buttonOne.on("click", function (event) {
+  pastTitle.text(capitalizeFirstLetter(savedExerciseTitle[0]));
 
-    pastInstructions.empty();
-      //create a new list with intructions
-      for (a = 0; a < savedExerciseInstructions[0].length; a++) {
-        var instruction = savedExerciseInstructions[0][a];
-        var instr = $("<li>");
-        instr.text(instruction);
-        pastInstructions.append(instr);
-      }
+  pastInstructions.empty();
+  //create a new list with intructions
+  for (a = 0; a < savedExerciseInstructions[0].length; a++) {
+    var instruction = savedExerciseInstructions[0][a];
+    var instr = $("<li>");
+    instr.text(instruction);
+    pastInstructions.append(instr);
+  }
 
-    pastGif.attr("src", savedExerciseGif[0]);
+  pastGif.attr("src", savedExerciseGif[0]);
+
+  pastExerciseSlider.removeClass("hidden");
 });
 
-buttonTwo.on('click', function(event) {
-    pastTitle.text(capitalizeFirstLetter(savedExerciseTitle[1]));
+buttonTwo.on("click", function (event) {
+  pastTitle.text(capitalizeFirstLetter(savedExerciseTitle[1]));
 
-    pastInstructions.empty();
-      //create a new list with intructions
-      for (a = 0; a < savedExerciseInstructions[1].length; a++) {
-        var instruction = savedExerciseInstructions[1][a];
-        var instr = $("<li>");
-        instr.text(instruction);
-        pastInstructions.append(instr);
-      }
+  pastInstructions.empty();
+  //create a new list with intructions
+  for (a = 0; a < savedExerciseInstructions[1].length; a++) {
+    var instruction = savedExerciseInstructions[1][a];
+    var instr = $("<li>");
+    instr.text(instruction);
+    pastInstructions.append(instr);
+  }
 
-    pastGif.attr("src", savedExerciseGif[1]);
+  pastGif.attr("src", savedExerciseGif[1]);
+
+  pastExerciseSlider.removeClass("hidden");
 });
 
-buttonThree.on('click', function(event) {
-    pastTitle.text(capitalizeFirstLetter(savedExerciseTitle[2]));
+buttonThree.on("click", function (event) {
+  pastTitle.text(capitalizeFirstLetter(savedExerciseTitle[2]));
 
-    pastInstructions.empty();
-      //create a new list with intructions
-      for (a = 0; a < savedExerciseInstructions[2].length; a++) {
-        var instruction = savedExerciseInstructions[2][a];
-        var instr = $("<li>");
-        instr.text(instruction);
-        pastInstructions.append(instr);
-      }
+  pastInstructions.empty();
+  //create a new list with intructions
+  for (a = 0; a < savedExerciseInstructions[2].length; a++) {
+    var instruction = savedExerciseInstructions[2][a];
+    var instr = $("<li>");
+    instr.text(instruction);
+    pastInstructions.append(instr);
+  }
 
-    pastGif.attr("src", savedExerciseGif[2]);
+  pastGif.attr("src", savedExerciseGif[2]);
+
+  pastExerciseSlider.removeClass("hidden");
 });
